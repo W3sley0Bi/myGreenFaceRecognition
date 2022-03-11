1c06f058dcc801e62617fb524029fb1d7d11e885
Wesley G. Obi <42078311+W3sley0Bi@users.noreply.github.com>
2022-03-11 19:23:27 +0100

    uloading files in folders. face recognitions working for just one person

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const { engine } = require ('express-handlebars');
 const routing = require('./routing')
 
+const upload = require('express-fileupload')
+
 // importing bodyParser for parsing request data
 const bodyParser = require('body-parser');
 
-
+//creating my application
 const app = express();
+
+//defining what port to use.
 const port = process.env.PORT || 3001;
 
 //
@@ -25,8 +29,20 @@ app.use(express.static(__dirname + '/public'))
 //passing data with a post
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(upload())
+
+app.get("/registration",  (req, res) =>{
+
+  res.sendFile(__dirname + './uploadFaces')
+})
 
 app.post("/registration",  (req, res) =>{
+  if(req.files.face){
+    let face = req.files.face
+    console.log(face.name)
+    face.mv('./uploadFaces/'+face.name)
+    
+  }
   console.log(req.body)
 })
 
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,33 +1,67 @@
 const video = document.getElementById('video')
 
+//laoding Models
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+ // faceapi.nets.faceExpressionNet.loadFromUri('/models')
   
 ]).then(startVideo)
 
 function startVideo() {
+  console.log('Models Loading Completed')
   navigator.getUserMedia(
     { video: {} },
     stream => video.srcObject = stream,
     err => console.error(err)
   )
+  recognizeFaces()
 }
 
-video.addEventListener('playing', () => {
+async function recognizeFaces() {
+
+const labeledDescriptors = await loadLabeledImages()
+const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.7)
+
+video.addEventListener('play', async () => {
+  console.log('Playing')
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
-  
   const displaySize = { width: video.width, height: video.height }
+  
   faceapi.matchDimensions(canvas, displaySize)
+  
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    
+    const results = resizedDetections.map((d) => {
+      return faceMatcher.findBestMatch(d.descriptor)
+  })
+  results.forEach( (result, i) => {
+      const box = resizedDetections[i].detection.box
+      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+      drawBox.draw(canvas)
+  })
   }, 100)
 })
+}
+
+function loadLabeledImages() {
+   const labels = ['Wesley Obi'] // for WebCam
+  return Promise.all(
+      labels.map(async (label)=>{
+          const descriptions = []
+          for(let i=1; i<=2; i++) {
+              const img = await faceapi.fetchImage(`../labeled_images/${label}/${i}.JPG`)
+              const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+              console.log(label + i + JSON.stringify(detections))
+              descriptions.push(detections.descriptor)
+          }
+          console.log(label+' Faces Loaded ! ')
+          return new faceapi.LabeledFaceDescriptors(label, descriptions)
+      })
+  )
+}
